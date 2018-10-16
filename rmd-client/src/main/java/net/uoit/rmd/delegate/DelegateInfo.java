e43d62bdd8fb570dde877099ed457d21d4faea90
BradleyWood <bradley.wood@uoit.net>
2018-10-16 01:39:15 -0400

    calculate method hash for delegates

--- rmd-client/src/main/java/net/uoit/rmd/delegate/DelegateInfo.java
+++ rmd-client/src/main/java/net/uoit/rmd/delegate/DelegateInfo.java
@@ -1,11 +1,53 @@
 package net.uoit.rmd.delegate;
 
+import jdk.internal.org.objectweb.asm.Type;
 import lombok.Data;
 
+import java.lang.reflect.Method;
+
+/**
+ * Represents callsite information for a delegate function
+ */
 public @Data class DelegateInfo {
 
     private final Class definingClass;
     private final String name;
     private final String signature;
+    private Integer hash;
 
+    public DelegateInfo(final Class clazz, final String name, final String signature) {
+        this.definingClass = clazz;
+        this.name = name;
+        this.signature = signature;
+    }
+
+    public DelegateInfo(final Method method) {
+        definingClass = method.getDeclaringClass();
+        name = method.getName();
+        signature = Type.getMethodDescriptor(method);
+        hash = method.hashCode();
+    }
+
+    /**
+     * The hash of the callsite
+     *
+     * @return The hash used for remote invocation
+     */
+    public int getMethodHash() {
+        if (hash != null)
+            return hash;
+
+        for (final Method declaredMethod : definingClass.getDeclaredMethods()) {
+            if (signature.equals(Type.getMethodDescriptor(declaredMethod))){
+                hash = declaredMethod.hashCode();
+                break;
+            }
+        }
+
+        if (hash == null) {
+            throw new RuntimeException("No such delegate: " + name + " " + signature);
+        }
+
+        return hash;
+    }
 }
