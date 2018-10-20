aa0397aeda3c83014c56191092a6f9ce64520e9a
BradleyWood <bradley.wood@uoit.net>
2018-10-19 22:06:39 -0400

    impl async kotlin delegates with callback function

--- rmd-kotlin/src/main/kotlin/net/uoit/rmd/delegate.kt
+++ rmd-kotlin/src/main/kotlin/net/uoit/rmd/delegate.kt
@@ -1,6 +1,11 @@
 package net.uoit.rmd
 
 import net.uoit.rmd.delegate.DelegateInfo
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+
+private var service = Executors.newCachedThreadPool()
 
 private fun <R> getMethodInfo(func: Function<R>): DelegateInfo {
     return DelegateInfo(func.javaClass, "invoke", "()Ljava/lang/Object;")
@@ -8,4 +13,16 @@ private fun <R> getMethodInfo(func: Function<R>): DelegateInfo {
 
 fun <R> delegate(del: () -> R): R {
     return Rmd.asDelegate(getMethodInfo(del)).invoke(del) as R
+}
+
+fun <R> async(del: () -> R): Future<R> {
+    return service.submit(Callable {
+        delegate(del)
+    })
+}
+
+infix fun <R> Future<R>.callback(callback: (result: R) -> Unit) {
+    service.submit {
+        callback.invoke(get())
+    }
 }
